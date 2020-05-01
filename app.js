8a53ee0dfa538faaf8ff3d20adb8ddef74bc47ca
Teresa Cho <tcho@cca.edu>
2020-04-30 19:32:58 -0700

    creates a front and back image variable created from DOM element

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //DOM OBJECTS
 const pokeName = document.querySelector('.poke-name');
 const pokeId = document.querySelector('.poke-id');
-
+const pokeFrontImage = document.querySelector('.poke-front-image');
+const pokeBackImage = document.querySelector('.poke-back-image');
 
 fetch('https://pokeapi.co/api/v2/pokemon/1')
   .then(res => res.json())
