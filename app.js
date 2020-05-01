4f5afde75900108f7676c234029b265b6371bf2d
Teresa Cho <tcho@cca.edu>
2020-04-30 19:30:50 -0700

    creates pokeID variable created from DOM element

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //DOM OBJECTS
 const pokeName = document.querySelector('.poke-name');
+const pokeId = document.querySelector('.poke-id');
+
 
 fetch('https://pokeapi.co/api/v2/pokemon/1')
   .then(res => res.json())
