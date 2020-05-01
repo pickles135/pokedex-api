da8b8c16a6b9014976736a289eed3ecfff33b2e7
Teresa Cho <tcho@cca.edu>
2020-04-30 19:34:57 -0700

    creates a poke type 1 and 2 variables from DOM elements

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const pokeName = document.querySelector('.poke-name');
 const pokeId = document.querySelector('.poke-id');
 const pokeFrontImage = document.querySelector('.poke-front-image');
 const pokeBackImage = document.querySelector('.poke-back-image');
+const pokeTypeOne = document.querySelector('.poke-type-one');
+const pokeTypeTwo = document.querySelector('.poke-type-two');
 
 fetch('https://pokeapi.co/api/v2/pokemon/1')
   .then(res => res.json())
