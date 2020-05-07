5ddb631dff8d586ca770e44d7281602bac5ecf91
Teresa Cho <tcho@cca.edu>
2020-05-07 14:32:34 -0700

    creates a capitalize function to capitalize first letter of pokemon name

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ const TYPES = [
 
 
 //Functions 
+const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
+
 const resetScreen = () => {
   mainScreen.classList.remove('hide');
   for(const type of TYPES) {
