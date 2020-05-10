44291a877b282fd06981fdf981e7d62b723e2ced
Teresa Cho <tcho@cca.edu>
2020-05-09 21:17:28 -0700

    moves the fetchPokeData further above the other functions

--- app.js
+++ app.js
@@ -35,6 +35,35 @@ const resetScreen = () => {
   };
 };
 
+const fetchPokeData = id => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  .then(res => res.json())
+    .then(data => {
+      resetScreen()
+  
+      const dataTypes = data['types'];
+      const dataFirstType = dataTypes[0];
+      const dataSecondType = dataTypes[1];
+      pokeTypeOne.textContent = capitalize(dataFirstType['type']['name']);
+      if(dataSecondType) {
+        pokeTypeTwo.classList.remove('hide');
+        pokeTypeTwo.textContent = capitalize(dataSecondType['type']['name']);
+      } else {
+        pokeTypeTwo.classList.add('hide');
+        pokeTypeTwo.textContent = '';
+      }
+      
+      mainScreen.classList.add(dataFirstType['type']['name']);
+      pokeName.textContent = capitalize(data['name']);
+      pokeId.textContent = '#' + data['id'].toString().padStart(3, '0');
+      pokeWeight.textContent = data['weight'];
+      pokeHeight.textContent = data['height'];
+  
+      pokeFrontImage.src = data['sprites']['front_default'] || '';
+      pokeBackImage.src = data['sprites']['back_default'] || '';
+    });
+}
+
 const fetchPokeList = url => {
   fetch(url)
   .then(res => res.json())
@@ -82,34 +111,6 @@ const handleListItemClick = (e) => {
   console.log(id);
 }
 
-const fetchPokeData = id => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-  .then(res => res.json())
-    .then(data => {
-      resetScreen()
-  
-      const dataTypes = data['types'];
-      const dataFirstType = dataTypes[0];
-      const dataSecondType = dataTypes[1];
-      pokeTypeOne.textContent = capitalize(dataFirstType['type']['name']);
-      if(dataSecondType) {
-        pokeTypeTwo.classList.remove('hide');
-        pokeTypeTwo.textContent = capitalize(dataSecondType['type']['name']);
-      } else {
-        pokeTypeTwo.classList.add('hide');
-        pokeTypeTwo.textContent = '';
-      }
-      
-      mainScreen.classList.add(dataFirstType['type']['name']);
-      pokeName.textContent = capitalize(data['name']);
-      pokeId.textContent = '#' + data['id'].toString().padStart(3, '0');
-      pokeWeight.textContent = data['weight'];
-      pokeHeight.textContent = data['height'];
-  
-      pokeFrontImage.src = data['sprites']['front_default'] || '';
-      pokeBackImage.src = data['sprites']['back_default'] || '';
-    });
-}
 
 //Adding Event Listener
 leftButton.addEventListener('click', handleLeftButtonClick);
