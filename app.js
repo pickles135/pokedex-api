1d11cf09c25e0f706278fe8d4b288e235f58485e
Teresa Cho <tcho@cca.edu>
2020-05-09 18:20:44 -0700

    passes url as a param for the fetch and replaces the original URL w/ the param

--- app.js
+++ app.js
@@ -35,38 +35,38 @@ const resetScreen = () => {
   };
 };
 
+const fetchPokeList = url => {
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    const { results, previous, next } = data;
+    prevUrl = previous;
+    nextUrl = next; 
+    
+    for(let i = 0; i < pokeListItems.length; i++) {
+      const pokeListItem = pokeListItems[i];
+      const resultData = results[i]
+      
+      if (resultData) {
+        const { name, url } = resultData;
+        const urlArray = url.split('/');
+        const id = urlArray[urlArray.length - 2];
+        pokeListItem.textContent = id + '. ' + capitalize(name); 
+      } else {
+        pokeListItem.textContent = '';
+      }
+    };
+    
+  });
+};
+
 const handleRightButtonClick = () => {
 
 };
 
-const fetchPokeList = () => {
-  fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20')
-    .then(res => res.json())
-    .then(data => {
-      const { results, previous, next } = data;
-      prevUrl = previous;
-      nextUrl = next; 
-  
-      for(let i = 0; i < pokeListItems.length; i++) {
-        const pokeListItem = pokeListItems[i];
-        const resultData = results[i]
-        
-        if (resultData) {
-          const { name, url } = resultData;
-          const urlArray = url.split('/');
-          const id = urlArray[urlArray.length - 2];
-          pokeListItem.textContent = id + '. ' + capitalize(name); 
-        } else {
-          pokeListItem.textContent = '';
-        }
-      };
-        
-    });
-};
-
 // Fetches data for left side of pokedex.
 fetch('https://pokeapi.co/api/v2/pokemon/250')
-  .then(res => res.json())
+.then(res => res.json())
   .then(data => {
     resetScreen()
 
